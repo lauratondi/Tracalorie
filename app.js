b6c5125ca3c4b95eac30a44ee09c431dd59b49bd
Lauramars <lauramars85@gmail.com>
2020-09-15 12:13:08 +0200

    Get & Populate Items

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function () {
 
   // Public Methods
   return {
+    getItems: function () {
+      return data.items;
+    },
     logData: function () {
       return data;
     },
@@ -30,8 +33,28 @@ const ItemCtrl = (function () {
 
 // UI CONTROLLER
 const UICtrl = (function () {
+  const UISelectors = {
+    itemList: '#item-list',
+  };
+
   // Public Methods
-  return {};
+  return {
+    populateItemList: function (items) {
+      let html = '';
+
+      items.forEach(function (item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                  <i class="edit-item fa fa-pencil"></i>
+                </a>
+              </li>`;
+      });
+
+      //   Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
 })();
 
 // APP CONTROLLER
@@ -39,7 +62,11 @@ const App = (function (ItemCtrl, UICtrl) {
   // Pubblic Methods
   return {
     init: function () {
-      console.log('Initializing App...');
+      //   Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      //   Populate list with items
+      UICtrl.populateItemList(items);
     },
   };
 })(ItemCtrl, UICtrl);
